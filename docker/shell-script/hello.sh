fc293b67e07dabfe590f976ad0e36460262218e6
Rushain Sovis <rsovis@wiley.com>
2025-06-09 11:39:08 +0530

    shell-script

---
+++ docker/shell-script/hello.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Colors
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+echo ""
+echo "${GREEN}"
+echo " _   _      _ _         "
+echo "| | | | ___| | | ___    "
+echo "| |_| |/ _ \ | |/ _ \   "
+echo "|  _  |  __/ | | (_) |  "
+echo "|_| |_|\___|_|_|\___/   "
+echo ""
+echo "Hello from my first Docker container!"
+echo "${NC}"
